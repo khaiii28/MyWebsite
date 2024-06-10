256acaea6ad9b5c3967ae430d54ed65aefc1fc35
khaiii28 <151891554+khaiii28@users.noreply.github.com>
2024-06-10 13:53:18 +0700

    Update scripts.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,20 +1,12 @@
-// script.js
+var cartItemCount = 0;
 
-// Hàm để thêm sản phẩm vào giỏ hàng
-function addToCart(productName, price) {
-    // Simulate adding the product to the cart
-    alert("Sản phẩm " + productName + " đã được thêm vào giỏ hàng với giá " + price + "đ!");
+function addToCart(productName) {
+    cartItemCount++;
+    updateCart();
+    alert("Sản phẩm " + productName + " đã được thêm vào giỏ hàng!");
 }
 
-// Hàm để hiển thị thông tin chi tiết của sản phẩm
-function showProductDetails(productName) {
-    // Simulate displaying the product details
-    alert("Chi tiết sản phẩm " + productName);
+function updateCart() {
+    var cartItems = document.getElementById("cartItems");
+    cartItems.textContent = "Số lượng sản phẩm trong giỏ hàng: " + cartItemCount;
 }
-
-// Hàm để xử lý sự kiện khi trang đã tải xong
-window.onload = function() {
-    // Đoạn mã JavaScript để thực hiện các chức năng khác khi trang đã tải
-    // Ví dụ: Hiển thị một cửa sổ chào mừng khi trang đã tải
-    alert("Chào mừng bạn đến với cửa hàng đồ chơi!");
-};
